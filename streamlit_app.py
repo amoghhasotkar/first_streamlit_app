671152d6e399f49a67ff9f2505dffd5ebe925a86
amoghhasotkar <106029098+amoghhasotkar@users.noreply.github.com>
2022-05-22 23:16:54 +0530

    Update streamlit_app.py
    specials menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.header('Breakfast Favs')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
