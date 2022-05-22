695339f437b853ea0678fcc7e2bf05acfa339fd7
amoghhasotkar <106029098+amoghhasotkar@users.noreply.github.com>
2022-05-22 23:16:22 +0530

    Update streamlit_app.py
    emojis and name of menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.title('Hello World!')
 
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Favs')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
