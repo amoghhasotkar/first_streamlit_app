370be2d556d65bb48b40e56b93c453edb5a3ebff
amoghhasotkar <106029098+amoghhasotkar@users.noreply.github.com>
2022-05-25 20:59:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,6 @@ add_my_fruit=streamlit.text_input('What fruit would you like information about',
 
 streamlit.write('Thanks for adding ',add_my_fruit)
 
-streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
